947f283f5a4c33b08d9734c423fb32ce00541ec4
Ricardo <rjdiazzh@gmail.com>
2019-07-29 23:28:06 -0500

    agregando css y otro componente

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React from 'react';
 
 import './App.css';
 
+function Informationbox(){
+  return(
+    <div id= "box">Información </div>
+  )
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      
-        HOLA
+        BIENVENIDO AQUI SERA VERA TU LISTA <Informationbox/> 
       </header>
     </div>
   );
